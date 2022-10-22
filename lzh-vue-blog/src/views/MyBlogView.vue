02e389bc5f1ac1f6784088d66fa5f1d2c4cdc4e8
Lzh <Lzh@qq.com>
2022-10-22 17:35:01 +0800

    完成我的博客页面设计；完成用户喜欢、收藏文章列表的展示

---
+++ lzh-vue-blog/src/views/MyBlogView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Header></Header>
+    <MyBlog></MyBlog>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import MyBlog from "@/components/MyBlog";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+export default {
+  name: "MyBlogView",
+  components: {
+    MyBlog,
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
