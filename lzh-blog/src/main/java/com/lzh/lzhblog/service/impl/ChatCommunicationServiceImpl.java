c57ac045f12eb86686441efb367a7a0c8d651265
Lzh <Lzh@qq.com>
2022-11-09 22:45:57 +0800

    修复聊天室功能bug，聊天记录按照id升序显示

--- lzh-blog/src/main/java/com/lzh/lzhblog/service/impl/ChatCommunicationServiceImpl.java
+++ lzh-blog/src/main/java/com/lzh/lzhblog/service/impl/ChatCommunicationServiceImpl.java
@@ -4,23 +4,12 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.lzh.common.domain.ResponseResult;
 import com.lzh.common.domain.entity.ChatCommunication;
-import com.lzh.common.domain.entity.User;
-import com.lzh.common.domain.enums.AppHttpCodeEnum;
 import com.lzh.common.domain.vo.MsgUserVo;
-import com.lzh.common.utils.RedisCache;
 import com.lzh.lzhblog.dao.ChatCommunicationMapper;
-import com.lzh.lzhblog.dao.UserMapper;
 import com.lzh.lzhblog.service.ChatCommunicationService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
-
-import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-
-import static com.lzh.lzhblog.constants.SysConstants.MSG_RECEIVE_FROM;
 
 /**
  * 聊天记录(ChatCommunication)表服务实现类
@@ -31,8 +20,6 @@ import static com.lzh.lzhblog.constants.SysConstants.MSG_RECEIVE_FROM;
 @Service("chatCommunicationService")
 public class ChatCommunicationServiceImpl extends ServiceImpl<ChatCommunicationMapper, ChatCommunication> implements ChatCommunicationService {
 
-    @Resource
-    private RedisCache redisCache;
 
     @Override
     public ResponseResult listCommunicationsByUserId(String fromUserName, String toUserName) {
@@ -42,7 +29,7 @@ public class ChatCommunicationServiceImpl extends ServiceImpl<ChatCommunicationM
                 .or(i -> i.eq(ChatCommunication::getFromName, toUserName)
                         .and(i1 -> i1.eq(ChatCommunication::getToName, fromUserName)));
 
-        queryWrapper.orderByAsc(ChatCommunication::getTime);
+        queryWrapper.orderByAsc(ChatCommunication::getId);
         List<ChatCommunication> communicationList = list(queryWrapper);
         return ResponseResult.okResult(communicationList);
     }
