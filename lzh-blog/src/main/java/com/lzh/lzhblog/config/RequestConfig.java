d44854cf9c9d5ab81db0ebab0b29cbe507826a55
陆之恒 <1203464373@qq.com>
2023-03-20 20:13:43 +0800

    Spring Boot 配置证书，实现HTTPS访问

---
+++ lzh-blog/src/main/java/com/lzh/lzhblog/config/RequestConfig.java
@@ -0,0 +1,48 @@
+package com.lzh.lzhblog.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ClassName RequestConfig
+ * 请求转发，当用户发起 HTTP 调用时，自动转发到 HTTPS 上
+ * @author LZH
+ * @date 2019-09-19 12:17:17
+ * @version 1.0
+ **/
+@Configuration
+public class RequestConfig {
+    @Bean
+    TomcatServletWebServerFactory tomcatServletWebServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint constraint = new SecurityConstraint();
+                constraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                constraint.addCollection(collection);
+                context.addConstraint(constraint);
+            }
+        };
+        factory.addAdditionalTomcatConnectors(createTomcatConnector());
+        return factory;
+    }
+
+    private Connector createTomcatConnector() {
+        Connector connector = new
+                Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        // Http 的请求端口为 8081
+        connector.setPort(8081);
+        connector.setSecure(false);
+        // https 的端口为 9999
+        connector.setRedirectPort(9999);
+        return connector;
+    }
+}
